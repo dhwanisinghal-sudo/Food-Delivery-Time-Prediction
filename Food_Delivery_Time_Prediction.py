33f834485d6da88d0cff4f77e6800da23645bd41
dhwanisinghal-sudo <dhwani.singhal@somaiya.edu>
2026-01-22 19:51:38 +0000

    Fix boolean error

--- Food_Delivery_Time_Prediction.py
+++ Food_Delivery_Time_Prediction.py
@@ -5,7 +5,7 @@
     "colab": {
       "provenance": [],
       "authorship_tag": "ABX9TyOjb5ABqozzONFbCCE2A0lV",
-      "include_colab_link": true
+      "include_colab_link": True
     },
     "kernelspec": {
       "name": "python3",
